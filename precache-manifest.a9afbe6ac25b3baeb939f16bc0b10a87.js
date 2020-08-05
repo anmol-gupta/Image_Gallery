2be1db475fc3da028d68f3e9a6512eca745b7cf1
Anmol Gupta <anmolify@gmail.com>
2020-08-06 03:32:32 +0530

    Updates

---
+++ precache-manifest.a9afbe6ac25b3baeb939f16bc0b10a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6cbd7c120d30b1db05e7b530086f13",
+    "url": "/Image_Gallery/index.html"
+  },
+  {
+    "revision": "42b25c49130b4344e566",
+    "url": "/Image_Gallery/static/css/main.e8df9d69.chunk.css"
+  },
+  {
+    "revision": "ccbf039c4b3562b98196",
+    "url": "/Image_Gallery/static/js/2.6d821192.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Image_Gallery/static/js/2.6d821192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42b25c49130b4344e566",
+    "url": "/Image_Gallery/static/js/main.b0f105fb.chunk.js"
+  },
+  {
+    "revision": "9fbee993bfb2dcdf893c",
+    "url": "/Image_Gallery/static/js/runtime-main.052dd447.js"
+  }
+]);
